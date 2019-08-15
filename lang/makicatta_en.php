ca1e1af66423e99407747212783b80b7d2ac495b
Julien Tessier <julien@cari.agency>
2019-08-15 08:54:13 +0400

    english translation

---
+++ lang/makicatta_en.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/champs_extras_core/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'tableau_de_bord' => 'Dashboard',
+	'bienvenue_sur_spip' => 'Welcome to SPIP!',
+	'rechercher_un_article' => 'Search articles:',
+	'rechercher_un_auteur' => 'Search authors:',
+	'interface_basee_sur_adminlte' => 'admin dashboard based on [AdminLTE->https://adminlte.io/]',
+	'attention' => 'Caution!',
+	'info_permalink' => 'Permalink:',
+	'bouton_creer' => 'Create',
+	'bouton_choisir_un_mot_cle' => 'Choose a keyword',
+);
---
+++ lang/paquet-makicatta_en.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisies/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'makicatta_titre' => 'Makicatta',
+	'makicatta_description' => 'Admin dashboard based on [AdminLTE->https://www.adminlte.io].',
+);
